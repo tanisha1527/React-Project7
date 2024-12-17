160972a60dcf33fb01d07458e0c7894e3ba2683c
tanisha1527 <vermatanisha620@gmail.com>
2024-12-17 15:53:52 +0530

    Add sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Sidebar/> 
+    </>
   )
 }
 
