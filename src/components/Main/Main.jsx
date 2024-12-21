a2555d822868fbeeb2b878f370411ac03516f9ac
tanisha1527 <vermatanisha620@gmail.com>
2024-12-21 23:41:10 +0530

    Add css propertise to cards

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -11,7 +11,7 @@ const Main = () => {
         </div>
         <div className="main-container">
            <div className="greet">
-              <p><span>Hello, Dev</span></p>
+              <p><span>Hello, Dev.</span></p>
               <p>How can I help you today?</p>
            </div>
            <div className="cards">
