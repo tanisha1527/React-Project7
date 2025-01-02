3006087f64d3e64d8eeacae5450a76f0e4a43168
tanisha1527 <vermatanisha620@gmail.com>
2025-01-03 00:51:05 +0530

    Add some new features

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -12,6 +12,10 @@ const ContextProvider = (props) => {
   const [loading, setLoading] = useState(false);
   const [resultData, setResultData] = useState("");
 
+  const delayPara = (index,nextWord) => {
+      
+  }
+
   const onSent = async (prompt) => {
 
 
@@ -20,7 +24,19 @@ const ContextProvider = (props) => {
       setShowResult(true)
       setRecentPrompt(input)
       const resultresponse = await run(input)
-      setResultData(resultresponse)
+      let responseArray = resultresponse.split("**");
+      let newResponse ;
+      for(let i =0 ; i < responseArray.length; i++)
+      {
+        if (i === 0 || i%2 !== 1) {
+          newResponse += responseArray[i];
+        }
+        else{
+            newResponse += "<b>"+responseArray[i]+"</b>";
+        }
+      }
+      let newResponse2 = newResponse.split("*").join("</br>")
+      setResultData(newResponse2)
       setLoading(false)
       setInput("")
   }
