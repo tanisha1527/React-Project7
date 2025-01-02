1e8e68db38f19e7d0cad048895406d68145d4388
tanisha1527 <vermatanisha620@gmail.com>
2025-01-02 21:41:01 +0530

    Add a new page to display the results

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,19 +1,41 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import run from "../config/gemini";
 
-const Context = createContext();
+export const Context = createContext();
 
 const ContextProvider = (props) => {
      
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompts, setPrevPrompts] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
 
   const onSent = async (prompt) => {
-      await run(prompt)
+
+
+      setResultData("")
+      setLoading(true)
+      setShowResult(true)
+      setRecentPrompt(input)
+      const resultresponse = await run(input)
+      setResultData(resultresponse)
+      setLoading(false)
+      setInput("")
   }
 
-  onSent("What is react js")
-
   const contextValue = {
-
+       prevPrompts,
+       setPrevPrompts,
+       onSent,
+       setRecentPrompt,
+       recentPrompt,
+       showResult,
+       loading,
+       resultData,
+       input,
+       setInput,
   }
 
   return(
